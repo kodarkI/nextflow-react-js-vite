618698c0dc161578857cc503885ed229c91ceba0
kodarkI <kodark084@gmail.com>
2025-07-02 09:05:41 +0000

    feat: add Hello and Goodbye components to enhance greeting functionality

--- src/App.jsx
+++ src/App.jsx
@@ -3,11 +3,48 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
-function App() {
-  let city = 'bangkok'
+
+
+
+const Hello = (props) => {
+  if (props.username === undefined) {
+    return (
+      <h1 className='danger'>Hello, guest</h1>
+    );
+  }
+
+  return (
+  <>
+  <h1 className='danger'>Hello_let {props.username}</h1>
+  <p>How are you</p>
+  </>
+  );
+}   
+
+function Goodbye() {
   return (
     <div>
+      <h1 className='danger'>Goodbye</h1>
+      <p>See you later</p>
+    </div>
+  );
+}
+
+function App() {
+  let city = 'bangkok'
+  let C_name = 'Udon'
+const PopA = () => {
+  console.info('PopA function was called.');
+  // Optionally, show a user notification here
+  // alert('Sign up for our tester program!');
+};
+
+  return (
+    <div>
+      <Hello username={C_name} />
       <h1 id="header" className='danger'>Hello, {city}</h1>
+      <Goodbye />
+      <button onClick={PopA}>Click here</button>
     </div>
   );  
 }
